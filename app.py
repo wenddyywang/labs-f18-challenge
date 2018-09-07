96c5d39401dd9f338632eb19203eca3b04e61755
Wendy Wang <www2105@columbia.edu>
2018-09-07 12:00:35 -0400

    Wendy Wang ADI Lab Challenge Complete

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -7,6 +8,23 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/pokemon/<var>')
+def pokemon(var):
+	url = "http://pokeapi.co/api/v2/pokemon/" + var + "/"
+	payload = ""
+	response = requests.request("GET", url, data=payload)
+
+	data = response.json()
+	if var.isdigit():
+		name = data['name']
+		search_val = 'id'
+		return render_template('pokemon.html', searchVal=search_val, name=name, idNum=var)
+		# return name
+	else:
+		id_num = data['id']
+		search_val = 'name'
+		# return id_num
+		return render_template('pokemon.html', searchVal=search_val, name=var, idNum=id_num)
 
 if __name__ == '__main__':
     app.run()
